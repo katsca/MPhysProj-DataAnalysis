d03c080f0f6edc98155a3128f7a17b2dcdd07d15
pfannkuchen2023 <katiescales203@gmail.com>
2026-02-04 12:15:04 +0000

    Initialized new project set up

---
+++ src/stabilizer-data-anlysis/fft.py
@@ -0,0 +1,9 @@
+'''
+This file is for plotting the FFT and PSD of the data collected from oscilloscope
+'''
+
+import argparse
+
+# Extract the data we want
+# For now hard code but will add argument parser for file name
+
